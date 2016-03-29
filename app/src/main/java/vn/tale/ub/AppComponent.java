5b129fc40a08be7f49fef48aa6600a509fc80970
Giang Nguyen <giangnguyen.tale@gmail.com>
2016-03-29 23:31:32 +0700

    remove files belong to UI

--- app/src/main/java/vn/tale/ub/AppComponent.java
+++ app/src/main/java/vn/tale/ub/AppComponent.java
@@ -1,13 +1,10 @@
 package vn.tale.ub;
 
 import dagger.Component;
-import vn.tale.ub.ui.list.UserListComponent;
-import vn.tale.ub.ui.list.UserListModule;
 
 /**
  * Author giangnguyen. Created on 3/29/16.
  */
 @Component(modules = { AppModule.class, ApiModule.class })
 public interface AppComponent {
-  UserListComponent plus(UserListModule module);
 }
