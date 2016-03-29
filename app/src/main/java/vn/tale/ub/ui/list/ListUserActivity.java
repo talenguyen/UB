5ea7e86f0dcc05d9cd0a053e83302be47a38c356
Giang Nguyen <giangnguyen.tale@gmail.com>
2016-03-29 22:33:42 +0700

    create feature user list

--- app/src/main/java/vn/tale/ub/ui/list/ListUserActivity.java
+++ app/src/main/java/vn/tale/ub/ui/list/ListUserActivity.java
@@ -1,12 +1,16 @@
-package vn.tale.ub;
+package vn.tale.ub.ui.list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import vn.tale.ub.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ListUserActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
   }
+
 }
---
+++ app/src/main/java/vn/tale/ub/ui/list/UserListApi.java
@@ -0,0 +1,7 @@
+package vn.tale.ub.ui.list;
+
+/**
+ * Author giangnguyen. Created on 3/29/16.
+ */
+public interface UserListApi {
+}
